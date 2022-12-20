43837808a5eb4d38a03b92d3e9ac42197c7dd094
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-12-20 03:34:37 +0100

    Refactor, rename & clean-up code.

--- monolith/src/main/kotlin/backend/Mails.kt
+++ monolith/src/main/kotlin/backend/Mails.kt
@@ -10,15 +10,12 @@ import backend.Constants.TITLE_KEY_PASSWORD
 import backend.Constants.TITLE_KEY_SIGNUP
 import backend.Constants.USER
 import backend.Log.log
-import backend.MailService.AbstractMailService
 import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Profile
 import org.springframework.mail.MailException
 import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.scheduling.annotation.Async
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.thymeleaf.context.Context
 import org.thymeleaf.spring5.SpringTemplateEngine
@@ -27,7 +24,7 @@ import javax.mail.MessagingException
 import kotlin.text.Charsets.UTF_8
 
 /*=================================================================================*/
-interface MailService{
+interface MailService {
     fun sendEmail(
         to: String,
         subject: String,
@@ -35,6 +32,7 @@ interface MailService{
         isMultipart: Boolean,
         isHtml: Boolean
     )
+
     fun sendPasswordResetMail(account: AccountCredentials)
     fun sendActivationEmail(account: AccountCredentials)
     fun sendCreationEmail(account: AccountCredentials)
@@ -44,48 +42,54 @@ interface MailService{
         titleKey: String
     )
 
-    abstract class AbstractMailService(    private val properties: ApplicationProperties,
-                                           private val messageSource: MessageSource,
-                                           private val templateEngine: SpringTemplateEngine):MailService{
-        override fun sendEmailFromTemplate(
-            account: AccountCredentials,
-            templateName: String,
-            titleKey: String
-        ) {
-            when (account.email) {
-                null -> {
-                    log.debug("Email doesn't exist for user '${account.login}'")
-                    return
-                }
 
-                else -> forLanguageTag(account.langKey).apply {
-                    sendEmail(
-                        account.email,
-                        messageSource.getMessage(titleKey, null, this),
-                        templateEngine.process(templateName, Context(this).apply {
-                            setVariable(USER, account)
-                            setVariable(BASE_URL, properties.mail.baseUrl)
-                        }),
-                        isMultipart = false,
-                        isHtml = true
-                    )
-                }
+}
+/*=================================================================================*/
+
+abstract class AbstractMailService(
+    private val properties: ApplicationProperties,
+    private val messageSource: MessageSource,
+    private val templateEngine: SpringTemplateEngine
+) : MailService {
+    override fun sendEmailFromTemplate(
+        account: AccountCredentials,
+        templateName: String,
+        titleKey: String
+    ) {
+        when (account.email) {
+            null -> {
+                log.debug("Email doesn't exist for user '${account.login}'")
+                return
+            }
+
+            else -> forLanguageTag(account.langKey).apply {
+                sendEmail(
+                    account.email,
+                    messageSource.getMessage(titleKey, null, this),
+                    templateEngine.process(templateName, Context(this).apply {
+                        setVariable(USER, account)
+                        setVariable(BASE_URL, properties.mail.baseUrl)
+                    }),
+                    isMultipart = false,
+                    isHtml = true
+                )
             }
         }
-
-        override fun sendActivationEmail(account: AccountCredentials) = sendEmailFromTemplate(
-            account, TEMPLATE_NAME_SIGNUP, TITLE_KEY_SIGNUP
-        ).run { log.debug("Sending activation email to '${account.email}'") }
-
-        override fun sendCreationEmail(account: AccountCredentials) = sendEmailFromTemplate(
-            account, TEMPLATE_NAME_CREATION, TITLE_KEY_SIGNUP
-        ).run { log.debug("Sending creation email to '${account.email}'") }
-
-        override fun sendPasswordResetMail(account: AccountCredentials) = sendEmailFromTemplate(
-            account, TEMPLATE_NAME_PASSWORD, TITLE_KEY_PASSWORD
-        ).run { log.debug("Sending password reset email to '${account.email}'") }
     }
+
+    override fun sendActivationEmail(account: AccountCredentials) = sendEmailFromTemplate(
+        account, TEMPLATE_NAME_SIGNUP, TITLE_KEY_SIGNUP
+    ).run { log.debug("Sending activation email to '${account.email}'") }
+
+    override fun sendCreationEmail(account: AccountCredentials) = sendEmailFromTemplate(
+        account, TEMPLATE_NAME_CREATION, TITLE_KEY_SIGNUP
+    ).run { log.debug("Sending creation email to '${account.email}'") }
+
+    override fun sendPasswordResetMail(account: AccountCredentials) = sendEmailFromTemplate(
+        account, TEMPLATE_NAME_PASSWORD, TITLE_KEY_PASSWORD
+    ).run { log.debug("Sending password reset email to '${account.email}'") }
 }
+
 /*=================================================================================*/
 @Async
 @Service
@@ -95,10 +99,10 @@ class MailServiceSmtp(
     private val mailSender: JavaMailSender,
     private val messageSource: MessageSource,
     private val templateEngine: SpringTemplateEngine
-) :MailService, AbstractMailService(
- properties,
- messageSource,
-templateEngine
+) : MailService, AbstractMailService(
+    properties,
+    messageSource,
+    templateEngine
 ) {
     override fun sendEmail(
         to: String,
@@ -126,38 +130,49 @@ templateEngine
             log.warn("Email could not be sent to user '$to'", e)
         }
     }
-
-
 }
 
 /*=================================================================================*/
-
-@Component
-class MailSenderSmtp(
-    private val properties: ApplicationProperties,
-    private val mailSender: JavaMailSender,
-    private val messageSource: MessageSource,
-    private val templateEngine: SpringTemplateEngine
-) //: JavaMailSenderImpl()
-
+//@Async
+//@Service
+//@Profile(MAILSLURP)
+//class MailServiceSlurp(
+//    private val properties: ApplicationProperties,
+//    private val messageSource: MessageSource,
+//    private val templateEngine: SpringTemplateEngine
+//) : MailService, AbstractMailService(
+//    properties,
+//    messageSource,
+//    templateEngine
+//) {
+//    override fun sendEmail(
+//        to: String,
+//        subject: String,
+//        content: String,
+//        isMultipart: Boolean,
+//        isHtml: Boolean
+//    ) {}
+//}
 /*=================================================================================*/
+//@Async
+//@Service
+//@Profile(GMAIL)
+//class MailServiceGmail(
+//    private val properties: ApplicationProperties,
+//    private val messageSource: MessageSource,
+//    private val templateEngine: SpringTemplateEngine
+//) : MailService, AbstractMailService(
+//    properties,
+//    messageSource,
+//    templateEngine
+//) {
+//    override fun sendEmail(
+//        to: String,
+//        subject: String,
+//        content: String,
+//        isMultipart: Boolean,
+//        isHtml: Boolean
+//    ) {}
+//}
 
-@Component
-@Profile(MAILSLURP)
-class MailSenderSlurp(
-    private val properties: ApplicationProperties,
-    private val mailSender: JavaMailSender,
-    private val messageSource: MessageSource,
-    private val templateEngine: SpringTemplateEngine
-) //: JavaMailSenderImpl()
-
-/*=================================================================================*/
-@Component
-@Profile(GMAIL)
-class MailSenderGmail(
-    private val properties: ApplicationProperties,
-    private val mailSender: JavaMailSender,
-    private val messageSource: MessageSource,
-    private val templateEngine: SpringTemplateEngine
-) //: MailService(properties, mailSender, messageSource, templateEngine)
 /*=================================================================================*/
